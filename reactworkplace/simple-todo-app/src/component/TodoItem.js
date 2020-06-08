c6a2ca7c5f0b25993c74adf2108f05a705b5a5ed
Chun-Yen Wang <cywang16@gmail.com>
2020-06-07 22:11:36 -0700

    part 2

--- reactworkplace/simple-todo-app/src/component/TodoItem.js
+++ reactworkplace/simple-todo-app/src/component/TodoItem.js
@@ -9,7 +9,17 @@ class TodoItem extends React.Component {
 */
 
 function TodoItem(props) {
-    return <li>{props.todo.title}</li>
+    return <li>
+      <input
+        type='checkbox'
+        checked={props.todo.completed}
+        onChange={() => props.handleChangeProps(props.todo.id)}
+	/>
+	<button onClick = {() => props.deleteTodoProps(props.todo.id)}>
+	Delete
+    </button>
+      {props.todo.title}
+    </li>
 }
 
 export default TodoItem
