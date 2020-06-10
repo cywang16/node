d989111e2b7d8cf90d3276035f36b03ab6978f7c
Chun-Yen Wang <cywang16@gmail.com>
2020-06-10 03:38:52 +0000

    start learning css

--- reactworkplace/simple-todo-app/src/index.js
+++ reactworkplace/simple-todo-app/src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom"
 
 // component file
 import TodoContainer from "./component/TodoContainer"
+import './App.css'
 ReactDOM.render(<TodoContainer />, document.getElementById("root"))
