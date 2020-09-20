1acc5d512a1f26e090dc8f968e348eee25876803
Chun-Yen Wang <cywang16@gmail.com>
2020-09-19 17:20:48 -0700

    React JSX

--- reactworkplace/w3school/src/index.js
+++ reactworkplace/w3school/src/index.js
@@ -44,4 +44,23 @@ const myElement = (
   </table>
 )
 
-ReactDOM.render(myElement, document.getElementById('myElements'))
+ReactDOM.render(myElement, document.getElementById('myElement'))
+
+// React JSX
+const myListElement = (
+  <ul>
+    <li>Apples</li>
+    <li>Bananas</li>
+    <li>Cherries</li>
+  </ul>
+)
+
+const groupedH1 = (
+  <div>
+    <h1>I am a Header.</h1>
+    <h1>I am a Header too.</h1>
+  </div>
+)
+
+ReactDOM.render(myListElement, document.getElementById('myListElement'))
+ReactDOM.render(groupedH1, document.getElementById('groupedH1'))
