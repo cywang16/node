a1045530708626c68ffce331f582c9a339ca7dd5
Chun-Yen Wang <cywang16@gmail.com>
2020-09-27 21:59:36 -0700

    fix indent

--- reactworkplace/w3school/src/components/renderHtml.js
+++ reactworkplace/w3school/src/components/renderHtml.js
@@ -4,17 +4,17 @@ var title = <h2>Component Render HTML</h2>
 
 const myElement = (
   <table>
-<tbody>
-    <tr>
-      <th>Name</th>
-    </tr>
-    <tr>
-      <td>John</td>
-    </tr>
-    <tr>
-      <td>Elsa</td>
-    </tr>
-</tbody>
+    <tbody>
+      <tr>
+        <th>Name</th>
+      </tr>
+      <tr>
+        <td>John</td>
+      </tr>
+      <tr>
+        <td>Elsa</td>
+      </tr>
+    </tbody>
   </table>
 )
 
