28b2bfe3be655e482295cab9740fa5144e2fdefd
Chun-Yen Wang <cywang16@gmail.com>
2020-09-28 18:36:48 +0000

    add selector

--- reactworkplace/w3school/src/components/SassHello.js
+++ reactworkplace/w3school/src/components/SassHello.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './SassHello.scss'
 
-var title = <h2>Hello Style!</h2>
-var paragraph = <p>Add a little style!</p>
+var title = <h2 className = 'SassHello'>Hello Style!</h2>
+var paragraph = <p className = 'SassHello'>Add a little style!</p>
 
 export default function SassHello() {
   return (
