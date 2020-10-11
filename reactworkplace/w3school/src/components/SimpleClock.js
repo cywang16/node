74045cb2585871613b4ba05e66a06276b320f777
Chun-Yen Wang <cywang16@gmail.com>
2020-10-11 12:54:05 -0700

    Style prop value must be an object

--- reactworkplace/w3school/src/components/SimpleClock.js
+++ reactworkplace/w3school/src/components/SimpleClock.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
 var radius = canvas.height / 2;
 ctx.translate(radius, radius);
 radius = radius * 0.90
@@ -9,14 +9,16 @@ drawClock();
 
 function drawClock() {
   ctx.arc(0, 0, radius, 0 , 2 * Math.PI);
-  ctx.fillStyle = "white";
+  ctx.fillStyle = 'white';
   ctx.fill();
 }
 
 class SimpleClock extends React.Component {
   render() {
     return (
-      <canvas id="canvas" width="400" height="400" style="background-color:#333" />
+      <canvas id = 'canvas'
+      width = '400' height = '400'
+      style = {{background-color: #333}} />
     )
   }
 }
