7ddcdaea096511dd2b21446472f9cfb4ed17d257
Chun-Yen Wang <cywang16@gmail.com>
2020-10-11 12:49:13 -0700

    Start working on simple clock

---
+++ reactworkplace/w3school/src/components/SimpleClock.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var radius = canvas.height / 2;
+ctx.translate(radius, radius);
+radius = radius * 0.90
+drawClock();
+
+function drawClock() {
+  ctx.arc(0, 0, radius, 0 , 2 * Math.PI);
+  ctx.fillStyle = "white";
+  ctx.fill();
+}
+
+class SimpleClock extends React.Component {
+  render() {
+    return (
+      <canvas id="canvas" width="400" height="400" style="background-color:#333" />
+    )
+  }
+}
+
+export default SimpleClock
--- reactworkplace/w3school/src/index.js
+++ reactworkplace/w3school/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SimpleClock from './components/SimpleClock'
 import App from './App'
+
+ReactDOM.render(
+  <SimpleClock />,
+  document.getElementById('SimpleClock')
+)
 
 ReactDOM.render(
   <App />,
