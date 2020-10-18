071aa732f6d747ec11e1744de1eeb97710a29b22
Chun-Yen Wang <cywang16@gmail.com>
2020-10-18 12:33:20 -0700

    fix error

--- reactworkplace/w3school/src/components/es6.js
+++ reactworkplace/w3school/src/components/es6.js
@@ -28,7 +28,7 @@ class Es6 extends React.Component {
         <td class = 'BorderedCell'>
           <p>The <strong>this</strong> keyword represents the Header object.</p>
           <button id = 'es6Btn' onClick = {myEs6header.checkTime}>Click Me!</button>
-          <p><strong>this</strong> was created at {myEs6header.date}</p><div id = 'es6Demo'></div>
+          <p><strong>this</strong> was created at {myEs6header.date.toString()}</p><div id = 'es6Demo'></div>
         </td>
       </tr>
     )
